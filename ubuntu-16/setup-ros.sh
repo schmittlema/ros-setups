50cbbaa789a26373474b62a6a971bc9a106c021b
katetolstaya <kate.tolstaya@gmail.com>
2017-05-11 15:20:23 -0400

    fixed a package name

--- ubuntu-16/setup-ros.sh
+++ ubuntu-16/setup-ros.sh
@@ -2,7 +2,7 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
 sudo apt-get update
 sudo apt-get -y install ros-kinetic-desktop
-sudo apt-get -y install ros-kinetic-gazebo-pkgs
+sudo apt-get -y install ros-kinetic-gazebo-ros-pkgs
 sudo rosdep init
 rosdep update
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
