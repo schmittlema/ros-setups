9379f34a0c04be31a403815c9f82d5f39b36a338
schmittlema <schmttle@udel.edu>
2017-06-08 10:42:45 -0400

    Create setup-install.sh

--- ubuntu-16/setup-install.sh
+++ ubuntu-16/setup-install.sh
@@ -3,27 +3,27 @@
 # Defaults        !tty_tickets
 
 export DEBIAN_FRONTEND=noninteractive
-sudo usermod -a -G dialout $USER
+usermod -a -G dialout $USER
 
-sudo add-apt-repository ppa:george-edison55/cmake-3.x -y
-sudo apt-get update && sudo apt-get upgrade
-sudo apt-get -q -y install cmake vim -y
-sudo apt-get -q -y install ant protobuf-compiler libeigen3-dev libopencv-dev
-sudo apt-get -q -y install python-argparse git-core wget zip python-empy qtcreator cmake build-essential genromfs -y
-sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+add-apt-repository ppa:george-edison55/cmake-3.x -y
+apt-get update && sudo apt-get upgrade
+apt-get -q -y install cmake vim -y
+apt-get -q -y install ant protobuf-compiler libeigen3-dev libopencv-dev
+apt-get -q -y install python-argparse git-core wget zip python-empy qtcreator cmake build-essential genromfs -y
+sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
 wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
-sudo apt-get update && sudo apt-get upgrade
+apt-get update && sudo apt-get upgrade
 # sudo apt-get install libignition-common-dev
-sudo apt-get -y install gazebo7
-sudo apt-get -y install libgazebo7-dev
+apt-get -y install gazebo7
+apt-get -y install libgazebo7-dev
 
 echo "export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:$HOME/src/Firmware/Tools/sitl_gazebo/Build" >> ~/.bashrc
 echo "export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:$HOME/src/Firmware/Tools/sitl_gazebo/models" >> ~/.bashrc
 echo "export GAZEBO_RESOURCE_PATH=${GAZEBO_MODEL_PATH}:$HOME/src/Firmware/Tools/sitl_gazebo/media" >> ~/.bashrc
-sudo apt-get remove -y gcc-arm-none-eabi gdb-arm-none-eabi binutils-arm-none-eabi
-sudo add-apt-repository ppa:team-gcc-arm-embedded/ppa
-sudo apt-get update
-sudo apt-get -q -y install python-serial openocd flex bison libncurses5-dev autoconf texinfo build-essential \
+apt-get remove -y gcc-arm-none-eabi gdb-arm-none-eabi binutils-arm-none-eabi
+add-apt-repository ppa:team-gcc-arm-embedded/ppa
+apt-get update
+apt-get -q -y install python-serial openocd flex bison libncurses5-dev autoconf texinfo build-essential \
     libftdi-dev libtool zlib1g-dev \
     python-empy gcc-arm-embedded -y
 
