d659a75853480dca0e80d8093565c603461afe07
schmittlema <schmttle@udel.edu>
2017-06-15 16:39:59 -0400

    Create setup-ros.sh

--- ubuntu-16/setup-ros.sh
+++ ubuntu-16/setup-ros.sh
@@ -1,6 +1,6 @@
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
-apt-get update
+apt-get update -y
 apt-get -y install ros-kinetic-desktop
 apt-get -y install ros-kinetic-gazebo-ros-pkgs
 rosdep init
