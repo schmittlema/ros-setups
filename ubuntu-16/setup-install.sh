db13507557cee6d3a34e297f875bc74b6fbc8720
schmittlema <schmttle@udel.edu>
2017-06-15 16:38:52 -0400

    Create setup-install.sh

--- ubuntu-16/setup-install.sh
+++ ubuntu-16/setup-install.sh
@@ -6,13 +6,13 @@ export DEBIAN_FRONTEND=noninteractive
 usermod -a -G dialout $USER
 
 add-apt-repository ppa:george-edison55/cmake-3.x -y
-apt-get update && apt-get upgrade
+apt-get update && apt-get upgrade -y
 apt-get -q -y install cmake vim -y
 apt-get -q -y install ant protobuf-compiler libeigen3-dev libopencv-dev
 apt-get -q -y install python-argparse git-core wget zip python-empy qtcreator cmake build-essential genromfs -y
 sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
 wget http://packages.osrfoundation.org/gazebo.key -O - | apt-key add -
-apt-get update && apt-get upgrade
+apt-get update && apt-get upgrade -y
 # sudo apt-get install libignition-common-dev
 apt-get -y install gazebo7
 apt-get -y install libgazebo7-dev
@@ -22,7 +22,7 @@ echo "export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:$HOME/src/Firmware/Tools/sit
 echo "export GAZEBO_RESOURCE_PATH=${GAZEBO_MODEL_PATH}:$HOME/src/Firmware/Tools/sitl_gazebo/media" >> ~/.bashrc
 apt-get remove -y gcc-arm-none-eabi gdb-arm-none-eabi binutils-arm-none-eabi
 add-apt-repository ppa:team-gcc-arm-embedded/ppa
-apt-get update
+apt-get update -y
 apt-get -q -y install python-serial openocd flex bison libncurses5-dev autoconf texinfo build-essential \
     libftdi-dev libtool zlib1g-dev \
     python-empy gcc-arm-embedded -y
