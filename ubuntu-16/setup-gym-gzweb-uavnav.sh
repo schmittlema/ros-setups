ef862f9dfadc782ed86f64f4b6aa5ba150a9452a
schmittlema <schmttle@udel.edu>
2017-06-15 16:17:26 -0400

    Create setup-gym-gzweb-uavnav.sh

---
+++ ubuntu-16/setup-gym-gzweb-uavnav.sh
@@ -0,0 +1,28 @@
+cd ~ && git clone https://github.com/openai/gym 
+cd gym && pip install -e . # minimal install
+pip install opencv-python
+
+apt-get install -y python-rospy python-genpy python-roslib
+
+apt-get update && apt-get upgrade -q -y && apt-get install -q -y \
+    build-essential \
+    cmake \
+    imagemagick \
+    libboost-all-dev \
+    libgts-dev \
+    libjansson-dev \
+    libtinyxml-dev \
+    mercurial \
+    nodejs \
+    nodejs-legacy \
+    npm \
+    pkg-config \
+    psmisc\
+&& rm -rf /var/lib/apt/lists/*
+
+cd ~; hg clone https://bitbucket.org/osrf/gzweb
+RUN cd ~/gzweb \
+    && hg up default \
+&& ./deploy.sh -m
+
+cd ~ &&git clone https://github.com/schmittlema/UAV-NAV.git
